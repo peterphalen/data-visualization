eb7e51007786e157a64a0b072299e372f8d9af26
Peter Phalen <PeterPhalen@Peters-MacBook-Air-2.local>
2016-07-04 03:19:05 -0400

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# data-visualization
-R script that dynamically generates a visualization of police killings using a json file hidden in The Counted website
+# Police killings
+R script that dynamically generates a visualization of police killings using a json file hidden in The Counted website. Conveys name, ethnicity, how they were killed, and whether they were armed at the time.
+
+See the resulting visualization here: https://www.peterphalen.com/datavisualization/
---
+++ map-police-killings.R
@@ -0,0 +1,69 @@
+
+#download needed packages you don't have 
+wants <- c("magrittr", "leaflet", "jsonlite", "curl")
+has   <- wants %in% rownames(installed.packages())
+if(any(!has)) install.packages(wants[!has])
+require(jsonlite)
+require(curl)
+require(leaflet)
+
+
+#pull data from json file embedded in the Guardian's The Counted website: http://www.theguardian.com/thecounted
+thecounted <- fromJSON("https://interactive.guim.co.uk/2015/the-counted/v/1455138961531/files/skeleton.json")
+
+
+#Color-code for whether the victim was armed
+#  Red = Unarmed
+#  Blue = armed
+#  Black = Don't know or ambiguous category like "Non-lethal firearm" or "vehicle"
+pal <- colorFactor(c(rep("blue",3), "red", rep("black",4)), domain= c("Disputed", 
+                                                                    "Firearm", 
+                                                                    "Knife", 
+                                                                    "No", 
+                                                                    "Non-lethal firearm",
+                                                                    "Other",
+                                                                    "Unknown",
+                                                                    "Vehicle"))
+
+#Use the leaflet htmlwidget to create an interactive online visualization of data
+leaflet(data = thecounted) %>%   #data from the counted
+
+#add default open source map tiles from OpenStreetMap
+addTiles() %>%  
+  
+#fit bounds around the USA
+fitBounds(-150,35, -70,50) %>%  
+
+#add a map legend
+addLegend(
+  title="People killed by police</br>(since Jan. 2015)",
+  position = 'bottomright',
+  colors = c("#FE1818","#5757D7", "#A9A7A3"), 
+  labels = c("Unarmed", "Armed", "Unknown / non-lethal / vehicle / other"))  %>%
+
+#dynamically add markers for people who were killed
+addCircleMarkers(~long, ~lat, radius = 5.5, stroke=FALSE, 
+color = ~pal(armed), #color defined above
+fillOpacity = ifelse(thecounted$armed=="No",0.9,0.3), #make unarmed dots more visible
+    
+    #create pop-up windows with some information for each marker
+    popup = ~ paste(sep="<br/>",name, 
+          #include race if available
+          ifelse(race == "B", "Black", 
+              ifelse(race == "W" , "White",
+                   ifelse(race =="H", "Hispanic",
+                        ifelse(race == "A", "Asian",
+                             ifelse(race == "N", "Native American",
+                                    ifelse(race == "U", "Race unknown", "")))))),
+          #include cause of death
+          ifelse(classification == "Gunshot", "Killed by gunshot",
+              ifelse(classification == "Death in custody", "Died in custody",
+                  ifelse(classification == "Other", "",
+                       ifelse(classification == "Taser", "Killed by taser",
+                            "Struck by vehicle")))),
+          #tell us whether they were unarmed or if unknown, else leave blank
+          #because the categories for being armed are convoluted
+          ifelse(armed=="No", "Unarmed", 
+               ifelse(armed=="Unknown", "Unknown if armed", "")))   )
+
+
