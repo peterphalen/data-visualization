f52a49ba37513cea047324c62d52285b6627bccb
Peter Phalen <PeterPhalen@Peters-MacBook-Air-2.local>
2016-07-06 00:04:14 -0400

    Update codebase, rm a+a code as will need to write regex to avoid changing url schemes in future

--- map-police-killings.R
+++ map-police-killings.R
@@ -7,9 +7,9 @@ require(curl)
 require(leaflet)
 require(magrittr)
 
-
 #pull data from json file embedded in the Guardian's The Counted website: http://www.theguardian.com/thecounted
-thecounted <- fromJSON("https://interactive.guim.co.uk/2015/the-counted/v/1455138961531/files/skeleton.json")
+thecounted <- fromJSON("https://interactive.guim.co.uk/2015/the-counted/v/1467749810058/files/skeleton.json")
+thecounted <- thecounted[thecounted$lat != "", ] #remove single entry that has missing lat/lang values, avoid later glitches
 
 #Color-code for whether the victim was armed
 #  Red = Unarmed
@@ -19,60 +19,96 @@ armedC <-  "#008080"
 #  Black = Don't know or ambiguous category like "Non-lethal firearm" or "vehicle"
 idkC <- "#000000"
 pal <- colorFactor(c(idkC, rep(armedC,2), unarmedC, rep(idkC,4)), domain= c("Disputed", 
-                                                                      "Firearm", 
-                                                                      "Knife", 
-                                                                      "No", 
-                                                                      "Non-lethal firearm",
-                                                                      "Other",
-                                                                      "Unknown",
-                                                                      "Vehicle"))
+                                                                            "Firearm", 
+                                                                            "Knife", 
+                                                                            "No", 
+                                                                            "Non-lethal firearm",
+                                                                            "Other",
+                                                                            "Unknown",
+                                                                            "Vehicle"))
 
-# automatically set date range for pulled data
-today <- Sys.Date()
-today <- format(today, format="%b %d %Y")
-dateRange <- paste0("(Jan 01 2015"," - ", today,")")
 
 #Use the leaflet htmlwidget to create an interactive online visualization of data
 leaflet(data = thecounted) %>%   #data from the counted
   
   #add default open source map tiles from OpenStreetMap
   addTiles() %>%  
-  
+    
   #fit bounds around the USA
-  fitBounds(-150,35, -70,50) %>%  
+  fitBounds(-125,25, -67,49) %>%  
   
   #add a map legend
   addLegend(
-    title=paste(sep="<br/>","People killed by police",dateRange),
+    title="People killed by police",
     position = 'bottomright',
     colors = c(unarmedC,armedC, idkC), 
     labels = c("Unarmed", "Armed", "Unknown / non-lethal / vehicle / other"))  %>%
   
   #dynamically add markers for people who were killed
   
-  addCircleMarkers(~long, ~lat, stroke=FALSE, 
+  ## 2015 only group (to allow user to select years)
+  addCircleMarkers(data=thecounted[as.Date(thecounted$date) <= as.Date("2015/12/31"),],~long, ~lat, stroke=FALSE, 
                    color = ~pal(armed), #color defined above
-                   fillOpacity = ifelse(thecounted$armed=="No",0.85,0.35), #make unarmed dots more visible
-                   
+                   fillOpacity = ~ifelse(armed=="No",0.75,0.3), #make unarmed dots more visible
                    #create pop-up windows with some information for each marker
-                   popup = ~ paste(sep="<br/>",name, 
+                   popup = ~ paste(name, "<br/>",
+                                   "Age",age,"<br/>",
                                    #include race if available
                                    ifelse(race == "B", "Black", 
                                           ifelse(race == "W" , "White",
                                                  ifelse(race =="H", "Hispanic",
                                                         ifelse(race == "A", "Asian",
                                                                ifelse(race == "N", "Native American",
-                                                                      ifelse(race == "U", "Race unknown", "")))))),
+                                                                      ifelse(race == "U", "Race unknown", "")))))),"<br/>",
+                                 
+                                   #tell us whether they were unarmed or if unknown, else leave blank
+                                   #because the categories for being armed are convoluted
+                                   ifelse(armed=="No", "Unarmed<br/>", 
+                                          ifelse(armed=="Unknown", "Unknown if armed<br/>",
+                                                  ifelse(armed=="Vehicle", "Armed with 'vehicle'<br/>",
+                                                    ifelse(armed=="Knife", "Had a knife<br/>",
+                                                      ifelse(armed=="Disputed", "Disputed if armed<br/>", ""))))),
+                   #include cause of death
+                   ifelse(classification == "Gunshot", "Killed by gunshot",
+                          ifelse(classification == "Death in custody", "Died in custody",
+                                 ifelse(classification == "Other", "",
+                                        ifelse(classification == "Taser", "Killed by taser",
+                                               ifelse(classification == "Struck by vehicle", "Struck by vehicle", "")))))),              
+                   group="2015")  %>%
+  
+  ## 2016 only group (to allow user to select years)
+  addCircleMarkers(data=thecounted[as.Date(thecounted$date) > as.Date("2015/12/31"),], ~long, ~lat, stroke=FALSE, 
+                   color = ~pal(armed), #color defined above
+                   fillOpacity = ~ifelse(armed=="No",0.75,0.3), #make unarmed dots more visible
+                   #create pop-up windows with some information for each marker
+                   popup = ~ paste(name, "<br/>",
+                                   "Age",age,"<br/>",
+                                   #include race if available
+                                   ifelse(race == "B", "Black", 
+                                          ifelse(race == "W" , "White",
+                                                 ifelse(race =="H", "Hispanic",
+                                                        ifelse(race == "A", "Asian",
+                                                               ifelse(race == "N", "Native American",
+                                                                      ifelse(race == "U", "Race unknown", "")))))),"<br/>",
+                                   
+                                   #tell us whether they were unarmed or if unknown, else leave blank
+                                   #because the categories for being armed are convoluted
+                                   ifelse(armed=="No", "Unarmed<br/>", 
+                                          ifelse(armed=="Unknown", "Unknown if armed<br/>",
+                                                 ifelse(armed=="Vehicle", "Armed with 'vehicle'<br/>",
+                                                        ifelse(armed=="Knife", "Had a knife<br/>",
+                                                               ifelse(armed=="Disputed", "Disputed if armed<br/>", ""))))),
                                    #include cause of death
                                    ifelse(classification == "Gunshot", "Killed by gunshot",
                                           ifelse(classification == "Death in custody", "Died in custody",
                                                  ifelse(classification == "Other", "",
                                                         ifelse(classification == "Taser", "Killed by taser",
-                                                               ifelse(classification == "Struck by vehicle", "Struck by vehicle", ""))))),
-                                   #tell us whether they were unarmed or if unknown, else leave blank
-                                   #because the categories for being armed are convoluted
-                                   ifelse(armed=="No", "Unarmed", 
-                                          ifelse(armed=="Unknown", "Unknown if armed", 
-                                                 ifelse(armed=="Disputed", "Disputed if armed", "")))) 
-    )
-
+                                                               ifelse(classification == "Struck by vehicle", "Struck by vehicle", "")))))),
+                   group="2016") %>%
+  
+  #give user the option of selecting years
+  addLayersControl(
+    overlayGroups = c("2015","2016"),
+    options = layersControlOptions(collapsed = FALSE)
+  ) 
+  
